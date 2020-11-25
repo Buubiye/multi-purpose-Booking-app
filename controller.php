84ee7b41dcac5e9be52fed34cd18644abac00c32
Buubiye <axmeda100@gmail.com>
2020-11-25 22:22:48 +0300

    Uploaded the first 2 base files for this app.

---
+++ controller.php
@@ -0,0 +1,30 @@
+<?php
+global $value;
+  class controller{
+	  // this variable 'value' stores values of the calculator
+	  // calculates the value
+	  public function calculator($num1, $num2, $sign, $getValue){
+		  switch ($sign){
+			  case 'Add':
+			    $getValue = $num1 + $num2;
+				break;
+		      case 'Subtract':
+			     $getValue = $num1 - $num2;
+				 break;
+			  Default:
+			  echo "no value found";
+	  }
+  }
+	  public function getValue(){
+			 return $value;
+	  }
+  }
+  
+  $numb1 = $_GET['num1'];
+  $numb2 = $_GET['num2'];
+  $signt = $_GET['sign'];
+  $calculator = new controller();
+  $calculator->calculator($numb1, $numb2, $signt);
+  echo $calculator->getValue();
+  include 'view.php';
+?>
