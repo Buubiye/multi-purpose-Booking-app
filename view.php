7d6556c90b3d0a93efb6a04f7fd9a3f28f07cd3e
Ahmed Nuur <axmeda100@gmail.com>
2020-11-30 22:10:15 +0300

    added a h3

--- view.php
+++ view.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "<form method='GET' action='controller.php'>
+echo "<h3> Calculate</h3><form method='GET' action='controller.php'>
          <input name='num1' placeholder='number1'/>
 		 <input name='num2' placeholder='number2'/>
 		 <select name='sign'>
