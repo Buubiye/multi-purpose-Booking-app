81e0a21b34afae4862d09f5e0daf605a77dabdc1
Ahmed Nuur <axmeda100@gmail.com>
2020-12-09 23:01:11 +0300

    created the database

--- model.php
+++ model.php
@@ -1,4 +1,23 @@
 <?php
+//create DB
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+try {
+  $conn = new PDO("mysql:host=$servername", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "CREATE DATABASE myDBPDO";
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  echo "Database created successfully<br>";
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+
 //This file will contain the model script 
 class model{
   public $user;
