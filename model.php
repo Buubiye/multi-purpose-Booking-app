1d3432fa0d6754f7b2a03d1aa3ceac2c658ee61b
Ahmed Nuur <axmeda100@gmail.com>
2020-12-10 23:58:15 +0300

    created connection with the db

--- model.php
+++ model.php
@@ -17,6 +17,20 @@ try {
 }
 
 $conn = null;
+//create connection
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
 
 //This file will contain the model script 
 class model{
