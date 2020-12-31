54f7fc9c9e1907338ae261145a6779fc64f539b3
Ahmed Nuur <axmeda100@gmail.com>
2020-12-31 22:50:52 +0300

    Update booking-plugin.php

--- booking-plugin.php
+++ booking-plugin.php
@@ -8,7 +8,7 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              Ahmed-Buubiye
+ * @link              Ahmed-C.Buubiye
  * @since             1.0.0
  * @package           Booking_Plugin
  *
@@ -17,8 +17,8 @@
  * Plugin URI:        Multi-Purpose-Booking-Plugin
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
- * Author:            Ahmed Buubiye
- * Author URI:        Ahmed-Buubiye
+ * Author:            Ahmed C.Buubiye
+ * Author URI:        Ahmed-C.Buubiye
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       booking-plugin
