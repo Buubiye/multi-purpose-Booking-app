d3f39c4356b639b9506dcdba2be3a23c35c36167
Ahmed Nuur <axmeda100@gmail.com>
2021-04-02 21:38:11 +0300

    Update booking-plugin-admin-display.php

--- admin/partials/booking-plugin-admin-display.php
+++ admin/partials/booking-plugin-admin-display.php
@@ -20,4 +20,15 @@
  $get_all_orders = $wpdb->get_results('SELECT * FROM wp_mpbp_orders');
  echo 'The Total Number of Orders are '. sizeof($get_all_orders) . ' Orders.';
  
+ // get the average stars for all services 
+ 
+ $get_all_ratings = $wpdb->get_results('SELECT sum(rating) AS totalsum FROM wp_mpbp_orders');
+ foreach($get_all_ratings as $result){
+	 $get_avarage_ratings = $result->totalsum / sizeof($get_all_orders);
+	 echo '<br> The average rating number is '. $get_avarage_ratings;
+ }
+ 
+ 
+ 
+
 ?>
