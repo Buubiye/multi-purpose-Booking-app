41276cd49c9f468f5cff6882cc7c5dfc13e4eb31
Ahmed Nuur <axmeda100@gmail.com>
2021-04-15 23:42:39 +0300

    Update booking-plugin-admin-display.php

--- admin/partials/booking-plugin-admin-display.php
+++ admin/partials/booking-plugin-admin-display.php
@@ -37,19 +37,58 @@
  }
  
  // get orders data by date
- $mpbp_date = '03-04-2021';
+ $mpbp_date = '07-04-2021';
  $get_data_by_date = array();
  $mpbp_store_dates = array();
  
+ /*
+ * Variables for Monthly, daily and yearly formats
+ *
+ * $date_format // stores the date format which is extracted
+ * $date_displayed // stores the date which will be used to match the labels
+ * $search_date_format // stores the date format for the searched date
+ * $search_starting_date // stores the starting date which searched
+ * $search_ending_date // stores the ending date which is searched 
+ */
+ if($_POST['mpbp_date_range'] == 'Daily' || $_POST['mpbp_date_range'] == 'Select Filter' | !isset($_POST['mpbp_date_range'])){
+	   $date_format = 'date, "%d"';
+	   $date_displayed = 'date, "%Y-%m-%d"';
+	   $search_date_format = 'tbl.date, "%d-%m-%Y"';
+	   $search_ending_date = date('d-m-Y');
+	   $search_starting_date = date('d-m-Y', strtotime($search_ending_date. ' - 7 Days'));
+ }else if($_POST['mpbp_date_range'] == 'Montly'){
+	   /*$date_format 
+	   $date_displayed 
+	   $search_date_format 
+	   $search_starting_date 
+	   $search_ending_date*/
+ }
+ 
   // stores the values for each date to be later displayed on the graphs
- $get_data = $wpdb->get_results('SELECT COUNT(DATE_FORMAT(date, "%d")) AS countOrders,
+ $get_data = $wpdb->get_results('SELECT COUNT(DATE_FORMAT('. $date_format .')) AS countOrders,
+							DATE_FORMAT('. $date_displayed .') AS getDate FROM wp_mpbp_orders tbl
+							WHERE DATE_FORMAT('. $search_date_format .') BETWEEN "'. $search_starting_date .'" 
+							AND "'. $search_ending_date .'" GROUP BY DAY(date)');
+  
+  echo $search_ending_date;
+  echo '<h1> hello </h1>';  
+  print_r($get_data);	
+  /*
+  * this is commented to be used as a backup
+    $get_data = $wpdb->get_results('SELECT COUNT(DATE_FORMAT(date, "%d")) AS countOrders,
 							DATE_FORMAT(date, "%Y-%m-%d") AS getDate FROM wp_mpbp_orders tbl
 							WHERE DATE_FORMAT(tbl.date, "%d-%m-%Y") BETWEEN "03-04-2021" AND "10-04-2021"
-                            GROUP BY DAY(date)');
-							
+                            GROUP BY DAY(date)');  
+   */
  for($i=0; $i<7; $i++){
+	 
 // increments the $mpbp_date date with a given number , 1 is minimum
- $mpbp_date = date('Y-m-d', strtotime($mpbp_date. ' + 1 days'));
+if($_POST['mpbp_date_range'] == 'Daily' | $_POST['mpbp_date_range'] == 'Select Filter' | !isset($_POST['mpbp_date_range'])){
+	$mpbp_date = date('Y-m-d', strtotime($mpbp_date. ' + 1 Days'));
+}else if($_POST['mpbp_date_range'] == 'Monthly'){
+	$mpbp_date = date('Y-M', strtotime($mpbp_date. ' - 1 Months'));
+}
+
 	 // matches the selected range with the fetched data
 	 if($mpbp_date == isset($get_data[$i]->getDate)){
 	 $get_data_by_date[$i] = $get_data[$i]->countOrders;
@@ -75,17 +114,17 @@ echo '<h1>'. $_POST['mpbp_date_range'] .'</h1>';
 
 
 // exmaple query ##### should be deleted later
-$lklk = $wpdb->get_results('SELECT COUNT(DATE_FORMAT(date, "%d")) AS getCount, DATE_FORMAT(date, "%d-%b") AS getDate FROM wp_mpbp_orders tbl
+/*$lklk = $wpdb->get_results('SELECT COUNT(DATE_FORMAT(date, "%d")) AS getCount, DATE_FORMAT(date, "%d-%b") AS getDate FROM wp_mpbp_orders tbl
 							WHERE DATE_FORMAT(tbl.date, "%d-%m-%Y") BETWEEN "03-04-2021" AND "10-04-2021"
                             GROUP BY DAY(date)');
 print_r($lklk);
 echo '<br><br><br>';
-echo JSON_encode($lklk);
+echo JSON_encode($lklk);*/
 ?>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.0.2/chart.min.js"></script>
 <form method="POST">
     <select name="mpbp_date_range">
-	   <option> <?php isset($_POST['mpbp_date_range'])? echo $_POST['mpbp_date_range'] : 'Select Filter' ?> </option>
+	   <option> <?php echo (isset($_POST['mpbp_date_range']))?  $_POST['mpbp_date_range'] : 'Select Filter'; ?> </option>
 	   <option> Daily </option>
 	   <option> Monthly </option>
 	   <option> Yearly </option>
