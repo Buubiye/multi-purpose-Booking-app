c41a9dd85392c46f1cb2583645ce0000070efcc8
Ahmed Nuur <axmeda100@gmail.com>
2021-04-19 23:41:02 +0300

    Update booking-plugin-admin-display.php

--- admin/partials/booking-plugin-admin-display.php
+++ admin/partials/booking-plugin-admin-display.php
@@ -37,10 +37,16 @@
  }
  
  // get orders data by date
- $mpbp_date = '07-04-2021';
+ $mpbp_date = date('d-m-Y', strtotime($search_ending_date. ' - 7 Days'));
  $get_data_by_date = array();
  $mpbp_store_dates = array();
  
+ // Stores the wpdb query variable for data extraction
+ $mpbp_query_data = array();
+ 
+ // 
+ $mpbp_this_day = date('d-m-Y');
+ $mpbp_this_month = date('m-Y');
  /*
  * Variables for Monthly, daily and yearly formats
  *
@@ -51,26 +57,32 @@
  * $search_ending_date // stores the ending date which is searched 
  */
  if($_POST['mpbp_date_range'] == 'Daily' || $_POST['mpbp_date_range'] == 'Select Filter' | !isset($_POST['mpbp_date_range'])){
-	   $date_format = 'date, "%d"';
-	   $date_displayed = 'date, "%Y-%m-%d"';
-	   $search_date_format = 'tbl.date, "%d-%m-%Y"';
-	   $search_ending_date = date('d-m-Y');
-	   $search_starting_date = date('d-m-Y', strtotime($search_ending_date. ' - 7 Days'));
- }else if($_POST['mpbp_date_range'] == 'Montly'){
-	   /*$date_format 
-	   $date_displayed 
-	   $search_date_format 
-	   $search_starting_date 
-	   $search_ending_date*/
+	 $mpbp_query_data = array(
+	   'date_format' => 'date, "%d"',
+	   'date_displayed' => 'date, "%Y-%m-%d"',
+	   'search_date_format' => 'tbl.date, "%d-%m-%Y"',
+	   'search_ending_date' => date('d-m-Y'),
+	   'search_starting_date' => date('d-m-Y', strtotime($mpbp_this_day. ' - 7 Days')),
+	   'group_by' => "DAY"
+	  );
+ }else if($_POST['mpbp_date_range'] == 'Monthly'){
+	 echo "<h1>here we are</h1>";
+	 $mpbp_query_data = array(
+	   'date_format' => 'date, "%m"',
+	   'date_displayed' => 'date, "%Y-%m"',
+	   'search_date_format' => 'tbl.date, "%m-%Y"',
+	   'search_ending_date' => date('m-Y'),
+	   'search_starting_date' => date('m-Y', strtotime($mpbp_this_month. ' - 7 Months')),
+	   'group_by' => "MONTH"
+	 );
  }
- 
+ print_r($mpbp_query_data);
   // stores the values for each date to be later displayed on the graphs
- $get_data = $wpdb->get_results('SELECT COUNT(DATE_FORMAT('. $date_format .')) AS countOrders,
-							DATE_FORMAT('. $date_displayed .') AS getDate FROM wp_mpbp_orders tbl
-							WHERE DATE_FORMAT('. $search_date_format .') BETWEEN "'. $search_starting_date .'" 
-							AND "'. $search_ending_date .'" GROUP BY DAY(date)');
+ $get_data = $wpdb->get_results('SELECT COUNT(DATE_FORMAT('. $mpbp_query_data["date_format"] .')) AS countOrders,
+							DATE_FORMAT('. $mpbp_query_data["date_displayed"] .') AS getDate FROM wp_mpbp_orders tbl
+							WHERE DATE_FORMAT('. $mpbp_query_data["search_date_format"] .') BETWEEN "'. $mpbp_query_data["search_starting_date"] .'" 
+							AND "'. $mpbp_query_data["search_ending_date"] .'" GROUP BY '. $mpbp_query_data["group_by"] .'(date)');
   
-  echo $search_ending_date;
   echo '<h1> hello </h1>';  
   print_r($get_data);	
   // convert $get_data stdclass object to array
@@ -91,10 +103,10 @@
  for($i=0; $i<7; $i++){
 	 
 // increments the $mpbp_date date with a given number , 1 is minimum
-if($_POST['mpbp_date_range'] == 'Daily' | $_POST['mpbp_date_range'] == 'Select Filter' | !isset($_POST['mpbp_date_range'])){
+if($_POST['mpbp_date_range'] == 'Daily' | $_POST['mpbp_date_range'] == 'Select Filter' | $_POST['mpbp_date_range'] == ''){
 	$mpbp_date = date('Y-m-d', strtotime($mpbp_date. ' + 1 Days'));
 }else if($_POST['mpbp_date_range'] == 'Monthly'){
-	$mpbp_date = date('Y-M', strtotime($mpbp_date. ' - 1 Months'));
+	$mpbp_date = date('Y-m', strtotime($mpbp_date. ' - 1 Months'));
 }
 
 
@@ -108,21 +120,6 @@ if($_POST['mpbp_date_range'] == 'Daily' | $_POST['mpbp_date_range'] == 'Select F
  }else{
 	 $get_data_by_date[$i] = 0;	
  };
-	/*foreach($get_data_converted as $key=>$val){
-		if($result->getDate == $mpbp_date){
-			echo '<h1>'. $key .'</h1>';  
-			$get_data_by_date[$i] = $get_data[$i]->countOrders;
-		}else{
-			$get_data_by_date[$i] = 0;	
-		}
-	}*/
-	 // matches the selected range with the fetched data
-	 /*if($mpbp_date == isset($get_data[$i]->getDate)){
-	 $get_data_by_date[$i] = $get_data[$i]->countOrders;
-	 }else{
-	 $get_data_by_date[$i] = 0;	 
-	 }*/
-	 
  
  
  // stores the labels 
@@ -131,14 +128,6 @@ if($_POST['mpbp_date_range'] == 'Daily' | $_POST['mpbp_date_range'] == 'Select F
  // stores dates extracted to be later diplayed on the charts
 	 $mpbp_store_dates[$i] = $custom_label;
 }
-
-echo JSON_encode($get_data_by_date);
-echo '<br><br>';
-echo JSON_encode($mpbp_store_dates[0]);
-echo '<h1>'. $_POST['mpbp_date_range'] .'</h1>';
-
-
-
 
 // exmaple query ##### should be deleted later
 /*$lklk = $wpdb->get_results('SELECT COUNT(DATE_FORMAT(date, "%d")) AS getCount, DATE_FORMAT(date, "%d-%b") AS getDate FROM wp_mpbp_orders tbl
@@ -152,9 +141,9 @@ echo JSON_encode($lklk);*/
 <form method="POST">
     <select name="mpbp_date_range">
 	   <option> <?php echo (isset($_POST['mpbp_date_range']))?  $_POST['mpbp_date_range'] : 'Select Filter'; ?> </option>
-	   <option> Daily </option>
-	   <option> Monthly </option>
-	   <option> Yearly </option>
+	   <option>Daily</option>
+	   <option>Monthly</option>
+	   <option>Yearly</option>
 	</select>
 	<button type="submit">Filter</button>
 </form>
